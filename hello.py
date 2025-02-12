1ff1dd4ef22ff2cf0054302faf23b8b0f970ddf8
Jhean Oliveira <jheancsouza.pt@.com>
2025-02-12 12:12:08 +0000

    ADICIONADO QUAL LINGUAGEM

--- hello.py
+++ hello.py
@@ -1,4 +1,9 @@
 #! /usr/bin/env python3
+"""Hello World Multi Linguas.
 
+
+Dependendo da lingua configurada no ambiente 
+o programa exibe a mensagem correspondente.
+"""
 # Este programa imprime Hello World
 print("Hello World")
